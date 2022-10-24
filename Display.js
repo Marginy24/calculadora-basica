cf823c45bdf9d86dc3dec83e422e9ac1fd056ba9
Marginy Uriarte <62030945+Marginy24@users.noreply.github.com>
2022-10-23 22:44:11 -0600

    Add files via upload

---
+++ Display.js
@@ -0,0 +1,45 @@
+class Display {
+    constructor(displayValorAnterior, displayValorActual) {
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculadora = new Calculadora ();
+        this.tipoOperacion =undefined;
+        this.ValorActual = '';
+        this.valorAnterior = '';
+    }
+    borrar() {
+        this.ValorActual = this.ValorActual.toString().slice(0,-1);
+        this.imprimirValores();
+    }
+    borrarTodo () {
+        this.ValorActual = '';
+        this.valorAnterior = '';
+        this.tipoOperacion = undefined;
+        this.imprimirValores();
+    }
+    computar(tipo) {
+        this.tipoOperacion !== 'igual' && this.calcular();
+        this.tipoOperacion = tipo;
+        this.valorAnterior = this.valorActual || this.valorAnterior;
+        this.valorActual = '';
+        this.imprimirValores();
+    }
+    agregarNumero(numero) {
+        if(numero === '.' && this.ValorActual.includes('.')) return
+        this.ValorActual = this.ValorActual.toString() + numero.toString();
+        this.imprimirValores();
+
+    }
+    imprimirValores () {
+        this.displayValorActual.textContent = this.ValorActual;
+        this.displayValorAnterior.textContent = this.ValorAnterior;
+    }
+    calcular() {
+        const valorAnterior = parseFloat(this.valorAnterior);
+        const ValorActual = parseFloat(this.valorActual);
+
+        if (isNaN(valorActual) || isNaN(valorAnterior) ) return
+        this.valorActual = this.calculadora[this.tipoOperacion] (valorAnterior, this.valorActual);
+
+    }
+}
